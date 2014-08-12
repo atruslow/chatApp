561a1f980a34a0fe94ed2af01b8850c83dae62dd
atruslow <alextruslow@gmail.com>
2014-08-12 08:37:58 -0400

    Added HTML/CSS and server side chat logic

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+var chatServer = require('./lib/chat_server');
 
 var cache = {};
 
@@ -16,12 +17,13 @@ var server = http.createServer(function(request,response){
 	}
 
 	var absPath = './' + filePath;
+	console.log('absPath:', absPath);
 	serveStatic(response, cache, absPath);
 
 }).listen(3000, function(){console.log('server listening on port 3000')});
 
 
-
+chatServer.listen(server);
 
 
 
@@ -46,7 +48,7 @@ function serveStatic (response, cache, absPath) {
 	} else {
 		fs.exists( absPath, function(exists) { 
 			if (exists) {
-				fs.read( absPath, function (err,data) {
+				fs.readFile( absPath, function (err,data) {
 					if (err) {
 						send404(response);
 					} else {
